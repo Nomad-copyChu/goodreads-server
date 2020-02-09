4502be749fe0b11111e22624cd01e2967a362bdc
jerrynim <tjerry3@naver.com>
2020-02-09 19:01:09 +0900

    [Add] rateBook API :building_construction:

--- src/Api/User/user.ts
+++ src/Api/User/user.ts
@@ -6,6 +6,32 @@ export default {
     bookComments: ({ id }) => prisma.user({ id }).bookComments(),
     shelves: ({ id }) => prisma.user({ id }).shelves(),
     likeQuotes: ({ id }) => prisma.user({ id }).likeQuotes(),
-    displays: ({ id }) => prisma.user({ id }).displays()
+    displays: ({ id }) => prisma.user({ id }).displays(),
+    bookAvgRating: async ({ id }) => {
+      const one = await prisma
+        .ratingsConnection({ where: { user: { id }, count: 1 } })
+        .aggregate()
+        .count();
+      const two = await prisma
+        .ratingsConnection({ where: { user: { id }, count: 2 } })
+        .aggregate()
+        .count();
+      const three = await prisma
+        .ratingsConnection({ where: { user: { id }, count: 3 } })
+        .aggregate()
+        .count();
+      const four = await prisma
+        .ratingsConnection({ where: { user: { id }, count: 4 } })
+        .aggregate()
+        .count();
+      const five = await prisma
+        .ratingsConnection({ where: { user: { id }, count: 5 } })
+        .aggregate()
+        .count();
+      return (
+        (one + two * 2 + three * 3 + four * 4 + five * 5) /
+        (one + two + three + four + five)
+      );
+    }
   }
 };
--- src/Api/book/rateBook/rateBook.ts
+++ src/Api/book/rateBook/rateBook.ts
@@ -1,31 +1,47 @@
 import { prisma } from "../../../generated/prisma-client";
+import { RateBookMutationArgs } from "../../../../types/types";
 
 export default {
   Mutation: {
-    rateBook: async (_, args, context) => {
+    rateBook: async (_, args: RateBookMutationArgs, context) => {
       const { user } = context;
       const { bookId, count } = args;
+      if (count < 0 || count > 5) {
+        throw Error("잘못된 별 개수 입니다.");
+      }
 
-      const existRating = await prisma.$exists.rating({
-        user: { id: user.id },
-        book: { id: bookId }
-      });
+      const Rating = await prisma
+        .user({ id: user.id })
+        .ratings({ where: { book: { id: bookId } } });
       //이미 rating 했다면 count 변경
-      if (existRating) {
-        await prisma.updateManyRatings({
-          where: {
-            user: { id: user.id },
-            book: { id: bookId }
-          },
+      const book = await prisma.book({ id: bookId });
+      if (Rating.length > 0) {
+        const rating = await prisma.updateRating({
+          where: { id: Rating[0].id },
           data: { count }
         });
+        await prisma.updateBook({
+          where: { id: bookId },
+          data: {
+            totalRating: book.totalRating - Rating[0].count + count
+          }
+        });
+        return rating;
       }
       //한적이 없다면 rating 생성
-      await prisma.createRating({
+      const rating = await prisma.createRating({
         user: { connect: { id: user.id } },
         book: { connect: { id: bookId } },
         count
       });
+      await prisma.updateBook({
+        where: { id: bookId },
+        data: {
+          totalRating: book.totalRating + count,
+          ratedUserNum: book.ratedUserNum + 1
+        }
+      });
+      return rating;
     }
   }
 };
