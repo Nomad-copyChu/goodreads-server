104bc693a9d5a06f008418ac868388b3cb95731d
jerrynim <tjerry3@naver.com>
2020-02-17 11:03:32 +0900

    [Fix] login token fix

--- src/server.ts
+++ src/server.ts
@@ -22,6 +22,7 @@ const server = new ApolloServer({
   context: async context => {
     //프리스마 에서 유저를 찾아 request 넣는다
     const token = context.req.headers.authorization;
+    console.log(token);
     if (token === "undefined" || !token) {
       return { ...context };
     } else {
