bfeb035475129000a617bbbb18d3910163b6fd47
jerrynim <tjerry3@naver.com>
2020-03-22 21:41:45 +0900

    Fix getQuotes

--- src/Api/Quote/getQuotes/getQuotes.ts
+++ src/Api/Quote/getQuotes/getQuotes.ts
@@ -3,7 +3,8 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Query: {
     getQuotes: async () => {
-      await prisma.quotes({ first: 10 });
+      const quotes = await prisma.quotes();
+      return quotes;
     }
   }
 };
