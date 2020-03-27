6b5fee94710e8b6df799b33f4212ad485191a7ac
jerrynim <tjerry3@naver.com>
2020-03-27 18:17:15 +0900

    fix user bookavgCount

--- src/Api/User/user.ts
+++ src/Api/User/user.ts
@@ -9,34 +9,12 @@ export default {
     displays: ({ id }) => prisma.user({ id }).displays(),
     profile: ({ id }) => prisma.user({ id }).profile(),
     bookAvgRating: async ({ id }) => {
-      //pg 연결에서 raw query를 날리면 훨씬 좋을듯
-      const [one, two, three, four, five] = await Promise.all([
-        await prisma
-          .ratingsConnection({ where: { user: { id }, count: 1 } })
-          .aggregate()
-          .count(),
-        await prisma
-          .ratingsConnection({ where: { user: { id }, count: 2 } })
-          .aggregate()
-          .count(),
-        await prisma
-          .ratingsConnection({ where: { user: { id }, count: 3 } })
-          .aggregate()
-          .count(),
-        await prisma
-          .ratingsConnection({ where: { user: { id }, count: 4 } })
-          .aggregate()
-          .count(),
-        await prisma
-          .ratingsConnection({ where: { user: { id }, count: 5 } })
-          .aggregate()
-          .count()
-      ]);
-
-      return (
-        (one + two * 2 + three * 3 + four * 4 + five * 5) /
-        (one + two + three + four + five)
-      ).toFixed(2);
+      const userRatings = await prisma.user({ id }).ratings();
+      let totalRating = 0;
+      userRatings.map(rating => {
+        totalRating += rating.count;
+      });
+      return (totalRating / userRatings.length).toFixed(2);
     }
   }
 };
